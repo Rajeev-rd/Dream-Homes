2aa25295e49340c038f9ba4e43561425ec2582de
Rajeev-rd <rajeevravindran02@gmail.com>
2024-03-13 16:00:29 +0530

    formauthenticated

--- Usermodule/urls.py
+++ Usermodule/urls.py
@@ -2,21 +2,17 @@ from django.urls import path
 from Usermodule import views
 from django.views.generic.base import RedirectView
 
+
 urlpatterns = [
     path('', RedirectView.as_view(pattern_name='indexfront', permanent=False)),
     path('indexfront',views.indexfront,name="indexfront"),
-    path('signup',views.signup,name="signup"),
-    path('logins',views.logins,name="logins"),
+    path('signup',views.RegistrationView.as_view(),name="signup"),
     path('about',views.about,name="about"),
     path('service',views.service,name="service"),
     path('projects',views.projects,name="projects"),
     path('contact',views.contact,name="contact"),
-    path('loginusers',views.loginusers,name="loginusers"),
-    path('myview',views.myview,name="myview"),
-    path('logout', views.logout, name="logout"),
-
-
-
-    path('check-authentication/', views.check_authentication, name="check_authentication"),
+    path('login',views.SignInView.as_view(),name="login"),
+    path('logout',views.signout_view,name="logout")
+    
 
 ]
--- Usermodule/views.py
+++ Usermodule/views.py
@@ -1,100 +1,62 @@
-from django.shortcuts import render,redirect
-from .models import RegistrationDb
-from django.core.exceptions import ValidationError
-from django.contrib.auth.models import User
-from Adminmodule.views import index
-from django.contrib.auth import authenticate,login
-from django.contrib import messages
 from django.http import JsonResponse
-from django.contrib.auth import logout as django_logout
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login,logout
+from django.contrib.auth.models import User
+from .forms import RegistrationForm, LoginForm
+from django.views.generic import View, FormView
 # Create your views here.
 
 # function for signup
-def signup(request):
-     if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        mobile = request.POST.get('mobile')
-        password = request.POST.get('password')
-        confirm_password = request.POST.get('confirm_password')
-        
-        # Check if password and confirm_password match
-        if password != confirm_password:
-            error_message = "Passwords do not match."
-            return render(request, 'signuppage.html', {'error_message': error_message})
-        
-        if RegistrationDb.objects.filter(email=email).exists():
-            return render(request,'signuppage.html',{'error_message':'Email already exists'})
-        
-        try:
-            # Attempt to create a new RegistrationDb instance
-            RegistrationDb.objects.create(
-                name=name,
-                email=email,
-                mobile=mobile,
-                password=password
-            )
-            # If successful, redirect to success page or any desired page
-            return redirect('indexfront')
-        except ValidationError as e:
-            # If validation error occurs due to uniqueness constraint, display error message
-            error_message = str(e)
-            return render(request, 'signuppage.html', {'error_message': error_message})
-     else:
-        return render(request,"signuppage.html")
+class RegistrationView(FormView):
+    def get(self, request, *args, **kwargs):
+        form = RegistrationForm()
+        return render(request, 'signuppage.html', {"form": form})
+
+    def post(self, request, *args, **kwargs):
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"registration successfully")
+            return redirect('login')
+        messages.error(request,"Please enter correct details")
+        return render(request, 'signuppage.html', {"form": form})
+
 #function for login
-def logins(request):
-    return render(request,"loginpage.html")
+class SignInView(View):
+    def get(self, request, *args, **kwargs):
+        form = LoginForm()
+        return render(request, 'loginpage.html', {"form": form})
 
-#function login admin and user
-def loginusers(request):
-    if request.method=='POST':
-        username =request.POST.get('username')
-        password = request.POST.get('password')
+    def post(self, request, *args, **kwargs):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                if user.is_superuser:
+                    # Redirect superusers to admin page
+                    return redirect('index')  # Replace 'admin_page' with your admin page URL name
+                else:
+                    return redirect('indexfront')  # Redirect regular users to index page
+            else:
+                messages.error(request, "Invalid credentials")
+        return render(request, 'loginpage.html', {"form": form})
 
-        if RegistrationDb.objects.filter(name=username,password=password).exists():
-            request.session['username']=username
-            request.session['password'] = password
-            request.session['is_admin'] = False
-            return redirect('indexfront')
-        elif User.objects.filter(username__contains=username).exists():
-             user=authenticate(username=username, password=password)
-             if user is not None:
-                 login(request,user)
-                 request.session['username'] = username
-                 request.session['password'] = password
-                 request.session['is_admin'] = True
-                 return redirect('index')
-             else:
-                 messages.error(request, "Incorrect password. Please try again.")
-                 return redirect('logins')
-        else:
-            messages.error(request, "Username is incorrect. Please try again.")
-            return redirect('logins')
-    else:
-        return redirect('logins')
-    
-def myview(request):
-    user_logged_in = RegistrationDb.objects.exists()
-    return render(request,"frontendindexpage.html", {'user_is_logged_in':user_is_logged_in})
 
-def check_authentication(request):
-    if request.user.is_authenticated:
-        return JsonResponse({'authenticated': True})
-    else:
-        return JsonResponse({'authenticated': False})
-    
-def logout(request):
-    # Log out the user using Django's logout function
-    django_logout(request)
-    return JsonResponse({'message': 'Logged out successfully'})
+
+def signout_view(request,*args, **kwargs):
+    logout(request)
+    return redirect("login")
 
 #function for index of user view
 def indexfront(request):
     return render(request,"frontendindexpage.html")
 
 #function for about page
+
 def about(request):
     return render(request,"about.html")
 
